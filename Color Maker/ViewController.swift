08c7ca7b45800ba7011bc7284be8b7648f99daba
mhy001 <mark.h.yang@gmail.com>
2016-06-08 20:08:55 -0700

    Add slider and disable switch

--- Color Maker/ViewController.swift
+++ Color Maker/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorView: UIView!
     
     override func viewDidLoad() {
@@ -20,9 +23,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func colorChange(sender: AnyObject) {
-        let r: CGFloat = redSwitch.on ? 1 : 0
-        let g: CGFloat = greenSwitch.on ? 1 : 0
-        let b: CGFloat = blueSwitch.on ? 1 : 0
+//        let r: CGFloat = redSwitch.on ? 1 : 0
+//        let g: CGFloat = greenSwitch.on ? 1 : 0
+//        let b: CGFloat = blueSwitch.on ? 1 : 0
+        let r: CGFloat = CGFloat(redSlider.value)
+        let g: CGFloat = CGFloat(greenSlider.value)
+        let b: CGFloat = CGFloat(blueSlider.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
